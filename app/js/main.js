8e7573ecc5b7e42bf3f9b280b6c99186567faed4
Ihor Larin <rmlaring@gmail.com>
2018-03-21 23:02:30 +0200

    js

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,23 @@
+window.onload = function () {
 
-var hamburger = document.querySelector('.hamburger');
-var navbar = document.querySelector('.navbar');
 
-hamburger.onclick = function() {
-  hamburger.classList.toggle('active');
-  navbar.classList.toggle('active');
+  var hamburger = document.querySelector('.hamburger');
+  var navbar = document.querySelector('.navbar');
+  var menuLink = document.querySelectorAll('.navbar .menu-list__link');
+
+  function menuToggle() {
+    hamburger.classList.toggle('active');
+    navbar.classList.toggle('active');
+  }
+
+  hamburger.onclick = menuToggle;
+  for (var i = 0; menuLink.length; i++) {
+    menuLink[i].onclick = function () {
+      menuToggle();
+    };
+  }
+
 };
+
+
+
